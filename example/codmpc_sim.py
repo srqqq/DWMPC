e6da1710b7a6cb201dadaa9c00713c3ec961dc0a
srqqq <srqqq@826835591@qq.com>
2025-10-10 16:36:30 +0800

    [特性新增] 使用local系下的角速度作为反馈

--- example/codmpc_sim.py
+++ example/codmpc_sim.py
@@ -66,8 +66,8 @@ while True:
         q = qpos[7:].copy()
 
         dp = qvel[:3].copy()
-        omega = env.base_configuration[:3,:3]@qvel[3:6]
-        # omega = qvel[3:6]
+        # omega = env.base_configuration[:3,:3]@qvel[3:6]
+        omega = qvel[3:6]
         dq = qvel[6:].copy()
 
         # q[0] = - q[0]
