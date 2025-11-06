5a36b754a1d185026175655b6209ed86e9d544b6
srqqq <srqqq@826835591@qq.com>
2025-11-06 23:56:40 +0800

    [特性新增] 优化控制效果，修复速度快时姿态误差大的问题

--- example/codmpc_sim.py
+++ example/codmpc_sim.py
@@ -6,7 +6,7 @@ from scipy.spatial.transform import Rotation as R
 from trajectory_planner import CircularTrajectoryPlanner
 
 robot_name = "go2"   # "aliengo", "mini_cheetah", "go2", "hyqreal", ...
-scene_name = "flat"  # "flat", "stairs", "ramp", "perlin", "random_boxes", "random_pyramids"
+scene_name = "perlin"  # "flat", "stairs", "ramp", "perlin", "random_boxes", "random_pyramids"
 state_observables_names = tuple(QuadrupedEnv.ALL_OBS)  # return all available state observables
 
 sim_frequency = 200.0
@@ -42,7 +42,8 @@ mpc_inerval = 1.0/mpc_frequency
 mpc_start_time = time.time()
 is_run_mpc = False
 
-is_traj_mode_enable = False # 想手动控制速度需要把它改为False
+is_traj_mode_enable = True # 想手动控制速度需要把它改为False
+# is_traj_mode_enable = False # 想手动控制速度需要把它改为False
 is_traj_start = False
 traj_start_time = time.time()
 
